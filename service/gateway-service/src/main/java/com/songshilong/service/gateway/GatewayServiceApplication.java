430ac909eb75809d5a6e4e8e4376382f16179f51
Ice, Song <ice.song@sap.com>
2025-11-27 17:32:05 +0800

    feat: 引入GatewayService

---
+++ service/gateway-service/src/main/java/com/songshilong/service/gateway/GatewayServiceApplication.java
@@ -0,0 +1,21 @@
+package com.songshilong.service.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @BelongsProject: chemical-platform-backend
+ * @BelongsPackage: com.songshilong.service.gateway
+ * @Author: Ice, Song
+ * @CreateTime: 2025-11-27  17:19
+ * @Description: GatewayApplication
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+}
